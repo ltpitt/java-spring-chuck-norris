7ba9a317956ceb314583e35464349b90d6da05b6
Davide Nastri <d.nastri@gmail.com>
2020-04-24 22:25:17 +0200

    feat: Added XML configuration example

--- src/main/java/it/davidenastri/javaspringchucknorris/JavaSpringChuckNorrisApplication.java
+++ src/main/java/it/davidenastri/javaspringchucknorris/JavaSpringChuckNorrisApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+// Added to enable XML configuration
 @ImportResource("classpath:joke-config.xml")
 public class JavaSpringChuckNorrisApplication {
 
--- src/main/java/it/davidenastri/javaspringchucknorris/config/JokeConfiguration.java
+++ src/main/java/it/davidenastri/javaspringchucknorris/config/JokeConfiguration.java
@@ -2,9 +2,11 @@ package it.davidenastri.javaspringchucknorris.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
+// Commented to enable XML configuration
 //@Configuration
 public class JokeConfiguration {
 
+    // Commented to enable XML configuration
     //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
